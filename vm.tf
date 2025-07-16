e22b03236c69909a42095a6db90f01adbe7b9b1b
solomon <ohiozeberyl2000@gmail.com>
2025-07-16 23:16:21 +0100

    Another pus

--- vm.tf
+++ vm.tf
@@ -138,14 +138,14 @@ resource "aws_security_group" "alb-sg" {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = [var.my_ip]
   }
 
   ingress {
     from_port   = 443
     to_port     = 443
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = [var.my_ip]
   }
 
   egress {
